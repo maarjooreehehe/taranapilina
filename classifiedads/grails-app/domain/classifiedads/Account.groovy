7fa679b54426b05c31d1678c4d93fdf12989fd13
maarjooreehehe <teesheerstories@yahoo.com>
2013-07-26 00:44:57 +0800

    Modified Register

--- classifiedads/grails-app/domain/classifiedads/Account.groovy
+++ classifiedads/grails-app/domain/classifiedads/Account.groovy
@@ -5,19 +5,13 @@ class Account {
 		String name
 		String username
 		String password
-		String address
 		String emailAddress
-		String phoneNumber
-		String gender
 	
     static constraints = {
 		name(blank:false)
 		username(blank:false)
 		password(blank:false)
-		address(blank:false)
 		emailAddress(blank:false)
-		phoneNumber(blank:false)
-		gender(inList:['Male', 'Female'],blank:false)
  }
 	
 }
